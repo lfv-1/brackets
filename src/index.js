d218063be74058ed3eaf3db595d01c8b77cd67dd
lfv-1 <nega123497@gmail.com>
2022-01-28 11:01:08 +0300

    feat:completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let arrRepeal = Object.fromEntries(bracketsConfig);
+  let arr = [];
+  for (let i of str.split('')) {
+    if (arr && arrRepeal[arr[arr.length-1]] == i) {
+      arr.pop(); 
+      } else { 
+        arr.push(...i); 
+      }
+  }
+    return arr.length == 0;
 }
